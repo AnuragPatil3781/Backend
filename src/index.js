d5b767ee2dedd92ecbac345cf66d13bda1169fbd
AnuragPatil3781 <anuragpatil3781@gmail.com>
2026-09-01 22:40:58 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({
-    path:'./env'
+    path:'./.qenv'
 })
 
-connectDB
+connectDB()
     .then(()=>{
         app.listen(process.env.PORT || 8000, ()=> {
             console.log(`Server is running at port: ${process.env.PORT}`);
