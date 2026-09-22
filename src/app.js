7e6210b80529f05da473e505354444bdd45c0580
AnuragPatil3781 <anuragpatil3781@gmail.com>
2026-09-22 11:31:19 +0530

    user login logout

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { app } from "./app.js";
 
 const app = express()
 
@@ -12,4 +13,5 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 export {app}
